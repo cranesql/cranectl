c61b7e0a4defd6398de604fa815ecfa0c040df22
Moritz Lang <16192401+slashmo@users.noreply.github.com>
2026-04-06 21:23:59 +0200

    Deploy Docker images to GHCR on push to main

--- Package.swift
+++ Package.swift
@@ -12,8 +12,14 @@ let package = Package(
         .executable(name: "crane", targets: ["CraneCTL"]),
         .library(name: "CraneCommands", targets: ["CraneCommands"]),
     ],
+    traits: [
+        .trait(name: "PostgresNIO", description: "Adds support for using the Crane Postgres NIO target."),
+        .default(enabledTraits: ["PostgresNIO"]),
+    ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+        .package(url: "https://github.com/cranesql/crane.git", branch: "main"),
+        .package(url: "https://github.com/cranesql/crane-postgres-nio.git", branch: "main"),
     ],
     targets: [
         .executableTarget(
@@ -27,7 +33,13 @@ let package = Package(
         .target(
             name: "CraneCommands",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Crane", package: "crane"),
+                .product(
+                    name: "CranePostgresNIO",
+                    package: "crane-postgres-nio",
+                    condition: .when(traits: ["PostgresNIO"])
+                ),
             ],
             swiftSettings: sharedSwiftSettings
         ),
